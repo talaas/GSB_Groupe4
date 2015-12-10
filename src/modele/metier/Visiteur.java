6c7c06ec2204e51bc6368a9fc57f4211994ccc35
laktati <anas_x96@hotmail.fr>
2015-12-10 17:49:14 +0100

    no message

---
+++ src/modele/metier/Visiteur.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modele.metier;
+
+/**
+ *
+ * @author Poulet
+ */
+public class Visiteur {
+    
+    private String matricule;
+    private String nom;
+    private String prenom;
+    private String adresse;
+    private int cp;
+    private String ville;
+    private String date_embauche;
+
+    public Visiteur(String matricule, String nom, String prenom, String adresse, int cp, String ville, String date_embauche) {
+        this.matricule = matricule;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.adresse = adresse;
+        this.cp = cp;
+        this.ville = ville;
+        this.date_embauche = date_embauche;
+    }
+
+    public String getMatricule() {
+        return matricule;
+    }
+
+    public void setMatricule(String matricule) {
+        this.matricule = matricule;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public int getCp() {
+        return cp;
+    }
+
+    public void setCp(int cp) {
+        this.cp = cp;
+    }
+
+    public String getVille() {
+        return ville;
+    }
+
+    public void setVille(String ville) {
+        this.ville = ville;
+    }
+
+    public String getDate_embauche() {
+        return date_embauche;
+    }
+
+    public void setDate_embauche(String date_embauche) {
+        this.date_embauche = date_embauche;
+    }
+    
+}
